0b9d0316f765c4744b452b4814386fb5c98b22be
vzirshehryar <vzirshehryar@gmail.com>
2024-04-16 01:09:41 +0500

    experience done

--- app/components/experiences/Experiences.tsx
+++ app/components/experiences/Experiences.tsx
@@ -26,15 +26,34 @@ const EXPERIENCES = [
   },
 ];
 
+type typeOfExperience = {
+  companyName: string;
+  jobTitle: string;
+  date: string;
+  tasks: string[];
+  logo: string;
+  left?: boolean;
+};
+
 const Experiences = () => {
   return (
-    <section className="my_section bg-primary2" id="EXPERIENCE">
-      <h1>Experience</h1>
-      <div className="pl-[20px]">
+    <section
+      className="my_section bg-primary2 grid place-items-center"
+      id="EXPERIENCE"
+    >
+      <h1 className="w-full">Experience</h1>
+      <div className="pl-[20px] md:hidden">
         <div className="border-l-white border-l-[2px] relative flex flex-col gap-5">
-          <Box {...EXPERIENCES[0]} />
-          <Box {...EXPERIENCES[1]} />
+          <MobileView {...EXPERIENCES[0]} />
+          <MobileView {...EXPERIENCES[1]} />
         </div>
+      </div>
+      <div className="relative hidden md:grid gap-5 grid-cols-2 max-w-[1200px]">
+        <div className="border-white border-[2px] absolute h-full left-[calc(50%-1px)]"></div>
+        <DesktopView {...EXPERIENCES[0]} left={true} />
+        <div></div>
+        <div></div>
+        <DesktopView {...EXPERIENCES[1]} left={false} />
       </div>
     </section>
   );
@@ -42,44 +61,63 @@ const Experiences = () => {
 
 export default Experiences;
 
-const Box = ({
-  companyName,
-  jobTitle,
-  date,
-  tasks,
-  logo,
-}: {
-  companyName: string;
-  jobTitle: string;
-  date: string;
-  tasks: string[];
-  logo: string;
-}) => {
+const MobileView = (props: typeOfExperience) => {
   return (
     <div className="relative">
       <div className="absolute left-[-24px] top-[30px] w-12 h-12 rounded-full -500 flex justify-center items-center border-white border-[2px]">
         <Image
           className="rounded-full"
-          src={logo}
+          src={props.logo}
           alt="logo"
           width={48}
           height={48}
         />
       </div>
-      <div className="bg-[#3A3F50] ml-8 p-6 rounded-2xl shadow-2xl relative">
-        <h3 className="mb-2">{jobTitle}</h3>
-        <div className="flex gap-2 items-center mb-1">
-          <h4 className="font-semibold text-[18px]">{companyName}</h4>
-          <span className="text-slate-300 text-[14px] ">{date}</span>
-        </div>
-        <ul>
-          {tasks.map((task, key) => (
-            <li key={key} className="list-disc ml-4">
-              {task}
-            </li>
-          ))}
-        </ul>
+      <Box {...props} />
+    </div>
+  );
+};
+
+const DesktopView = (props: typeOfExperience) => {
+  return (
+    <div className={`relative`}>
+      <div
+        className={`absolute ${
+          props.left ? "right-[-42px]" : "left-[-42px]"
+        } top-[30px] w-16 h-16 rounded-full -500 flex justify-center items-center border-white border-[2px]`}
+      >
+        <Image
+          className="rounded-full"
+          src={props.logo}
+          alt="logo"
+          width={64}
+          height={64}
+        />
       </div>
+      <Box {...props} />
+    </div>
+  );
+};
+
+const Box = (props: typeOfExperience) => {
+  return (
+    <div
+      className={`bg-[#3A3F50] ${
+        props.left ? "mr-8" : "ml-8"
+      } p-6 rounded-2xl shadow-2xl relative`}
+    >
+      <h3 className="mb-2">{props.jobTitle}</h3>
+      <div className="flex gap-2 items-center mb-1">
+        <h4 className="font-semibold text-[18px]">{props.companyName}</h4>
+        <span className="text-slate-300 text-[14px] ">{props.date}</span>
+      </div>
+      <ul>
+        {props.tasks.map((task, key) => (
+          <li key={key} className="list-disc ml-4">
+            {task}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
